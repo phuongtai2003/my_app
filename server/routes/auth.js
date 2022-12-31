9ecc9072e91e46d26befc42ddbc3f5c19386c4fb
Phuong Tai <tai.nguyenphuong12@gmail.com>
2022-12-31 10:30:49 +0700

    Some changes for the auth feature

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,11 +6,11 @@ const authRouter = express.Router();
 authRouter.post("/api/signup", async (req, res) => {
   try {
     const { email, name, password } = req.body;
-    const existingUser = User.findOne({ email: email });
+    const existingUser = await User.findOne({ email: email });
     if (existingUser) {
       return res.status(400).json({ msg: "Email has been used already" });
     }
-    const hashedPassword = bcryptjs.hash(password, 8);
+    const hashedPassword = await bcryptjs.hash(password, 8);
     let newUser = new User({
       name: name,
       password: hashedPassword,
